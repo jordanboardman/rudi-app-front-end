04b562707e78a4134e3aeccd67d75891c413def2
Matt Summers <mtsummersx@gmail.com>
2022-11-15 16:24:48 -0500

    minor changes to navbar and routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import {legacy_createStore as createStore} from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
